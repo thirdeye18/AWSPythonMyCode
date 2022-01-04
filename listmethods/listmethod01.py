d2cf31543de1d37bc08bb12d7e670e34136903b7
Justin Hammel <justinhammel62@gmail.com>
2022-01-04 22:12:56 +0000

    manipulating lists

---
+++ listmethods/listmethod01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# create list
+proto = ["ssh", "http", "https"]
+# print entire list
+print(proto)
+# print second list item
+print(proto[1])
+# line add d, n, s
+proto.extend("dns")
+print(proto)
