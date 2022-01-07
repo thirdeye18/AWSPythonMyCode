a3683a563e24c838665a7d9c25b762368cb72592
Justin Hammel <justinhammel62@gmail.com>
2022-01-07 18:01:13 +0000

    finished testing prime

--- challenges/challenge1.py
+++ challenges/challenge1.py
@@ -40,7 +40,9 @@ def prime_checker(num):
     # loop from 2 to num checking if it is divisible by other numbers
     for i in range(2, num):
         if num % i == 0:
+            print(f"{num} is not a prime number")
             return False
+    print(f"{num} is a prime number")
     return True
 
 """ splits a string into individual characters, remove white spaces, convert to lower
@@ -60,6 +62,13 @@ def main():
     sampleString2 = "This is a test string"
     panagram_check(sampleString1)
     panagram_check(sampleString2)
+
+    # testing prime_checker
+    prime_checker(1)
+    prime_checker(2)
+    prime_checker(29)
+    prime_checker(3.14)
+
 
 # this condition is only true if our script is run directly
 # it is NOT true if our code is imported into another script
